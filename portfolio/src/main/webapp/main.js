2eedb3fdd670053061c531b28c935325b014bcea
Carol Sun <carolsun5@gmail.com>
2020-07-08 20:56:36 +0000

    Implement comment feature into new website

--- portfolio/src/main/webapp/main.js
+++ portfolio/src/main/webapp/main.js
@@ -28,3 +28,62 @@ $(document).ready(function () {
     });
 });
 
+document.addEventListener("DOMContentLoaded", function() {
+  loadComments();
+});
+
+/**
+ * Load a specified number of comments to the comments section. If the specified
+ * number is greater than the total number of comments, all comments are shown.
+ */
+function loadComments() {
+    var numCommentsShown = parseInt(document.getElementById('comment-choice').value);
+    console.log(numCommentsShown);
+    fetch("/data?limit=".concat(numCommentsShown))
+    .then(response => response.json())
+    .then((messages) => {
+        const messageListElement = document.getElementById('comments-container');
+        messageListElement.innerHTML = "";
+        messages.forEach((comment) => {
+            messageListElement.appendChild(createCommentElement(comment));
+        });
+    });
+}
+
+/**
+ * Create comment on the page with a delete button. After a comment is deleted, 
+ * the comments are reloaded with the correct number of comments shown.
+ * 
+ * This functions creates HTML element that has the format:
+ *
+ *    <li class="comment">
+ *      <span>COMMENT_TEXT</span>
+ *      <button>Delete</button>
+ *    </li>
+ */
+function createCommentElement(comment) {
+    const commentElement = document.createElement('li');
+    commentElement.className = 'comment';
+
+    const textElement = document.createElement('span');
+    textElement.innerText = comment.text;
+
+    const deleteButtonElement = document.createElement('button');
+    deleteButtonElement.innerText = 'Delete';
+    deleteButtonElement.addEventListener('click', () => {
+        deleteComment(comment);
+        commentElement.remove();
+        fetch("/data?limit=10").then(loadComments()) ;
+    });
+
+    commentElement.appendChild(textElement);
+    commentElement.appendChild(deleteButtonElement);
+    return commentElement;
+}
+
+/** Tells the server to delete the comment. */
+function deleteComment(comment) {
+    const params = new URLSearchParams();
+    params.append('id', comment.id);
+    fetch('/delete-data', {method: 'POST', body: params});
+}
