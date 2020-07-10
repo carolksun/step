fc1c4e06ca0ace6ac9d51ee75ea4479555948dd5
Carol Sun <carolsun5@gmail.com>
2020-07-10 17:12:06 +0000

    Passes all tests

--- walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
+++ walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
@@ -15,9 +15,78 @@
 package com.google.sps;
 
 import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashSet;
+import java.util.TreeSet;
+import java.util.List;
+import java.util.ArrayList;
 
 public final class FindMeetingQuery {
+  private Collection<TimeRange> intervalFinder(List<Event> events_list, MeetingRequest request, Collection<TimeRange> possibleTimes)  {
+    Collection<String> attendees = request.getAttendees();
+    long duration = request.getDuration();
+
+    int prev_end = 0;
+      for (Event e : events_list){
+          Collection<String> intersection = new HashSet<>(attendees);
+          intersection.retainAll(e.getAttendees());
+          TimeRange range = e.getWhen();
+          if (intersection.size() > 0){
+              if (duration <= range.start() - prev_end){
+                  possibleTimes.add(TimeRange.fromStartEnd(prev_end, range.start(), false));
+              }
+              if (prev_end < range.end()){
+                  prev_end = range.end();
+              }
+          }
+      }
+      if (prev_end != TimeRange.END_OF_DAY + 1){
+          possibleTimes.add(TimeRange.fromStartEnd(prev_end, TimeRange.END_OF_DAY, true));
+      }
+
+      return possibleTimes;
+  }
+
+  // runtime: O(n log n) + O(n^2) = O(n^2)
   public Collection<TimeRange> query(Collection<Event> events, MeetingRequest request) {
-    throw new UnsupportedOperationException("TODO: Implement this method.");
+    long duration = request.getDuration();
+ 
+    // get mandatory and optional attendees
+    Collection<String> attendees = request.getAttendees();
+    Collection<String> optional = request.getOptionalAttendees();
+    
+    // sort the events to be in order by start time O(n log n)
+    List<Event> events_list = new ArrayList<Event>(events); 
+    Collections.sort(events_list); 
+
+    List<TimeRange> possibleTimes = new ArrayList<>();
+
+    // duration of meeting is wrong then no meeting times
+    if (duration > TimeRange.getTimeInMinutes(23, 59) || duration < 0){
+        return possibleTimes;
+    }
+    
+    // if no events for the day
+    if (events.size() == 0){
+        possibleTimes.add(TimeRange.WHOLE_DAY);
+        return possibleTimes;
+    }
+    
+    // intersection - O(n)
+    // For loop rutime - O(n^2)
+    if (optional.size() > 0){
+      List<String> allAttendees = new ArrayList<>(attendees);
+      allAttendees.addAll(optional);
+      MeetingRequest new_request = new MeetingRequest(allAttendees, duration);
+      Collection<TimeRange> slotsOptional = intervalFinder(events_list, new_request, possibleTimes);
+      if (slotsOptional.size() > 0){
+        return slotsOptional;
+      }
+      else if (attendees.size() == 0){
+        return possibleTimes;
+      }
+    }
+    return intervalFinder(events_list, request, possibleTimes);
   }
 }
